544e7741dec7cc1d31cf46847a04e78231117ef3
Albert Bou Pinar <alboup@gmail.com>
2023-05-29 11:51:51 +0200

    Sprint 4. Movies
    Nivell 1

--- src/films.js
+++ src/films.js
@@ -1,46 +1,72 @@
 // Exercise 1: Get the array of all directors.
+const x = 3;
+console.log(x);
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
-  return result;
+  return array.map((movie) => movie.director);
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  return array.filter((movie) => movie.director === director);
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
+
 function moviesAverageOfDirector(array, director) {
-  
+  const result = getMoviesFromDirector(array, director);
+  var mostrarPuntuacion = result.reduce(function(acumulador, siguienteValor) {
+      return {
+          score: acumulador.score + siguienteValor.score
+      };
+  }, { score: 0 });
+  var media = Number((mostrarPuntuacion.score / result.length).toFixed(2));
+  return media;
 }
 
-// Exercise 4:  Alphabetic order by title 
+// Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {
-  
+  return array
+    .map((movie) => movie.title)
+    .sort()
+    .slice(0, 20);
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let ordre = [...array].sort((a, b) =>
+    a.year > b.year
+      ? 1
+      : b.year > a.year
+      ? -1
+      : a.title > b.title
+      ? 1
+      : b.title > a.title
+      ? -1
+      : 0
+  );
+  return ordre;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
+function moviesAverageByCategory(array, category) {
+  let result = array.filter((movie) =>
+      movie.genre.includes(category)
+  );
+  var mostrarPuntuacion = result.reduce(function(acumulador, siguienteValor) {
+      return {
+          score: acumulador.score + siguienteValor.score
+      };
+  }, { score: 0 });
+  let numberOfMoviesWithScore = result.filter(movie => movie.score).length
+  var media = (mostrarPuntuacion.score / numberOfMoviesWithScore);
+  return media;
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+function hoursToMinutes() {}
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
-
-
+function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -53,6 +79,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
